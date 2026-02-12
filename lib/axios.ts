933a697c28eb5b745142d53bbc631c5db2da0e48
Esubalew Teshager <esuba419@gmail.com>
2026-02-12 09:36:50 +0300

    refactor(axios): restructure axios instance creation and export for improved readability

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,29 @@
 import axios from "axios";
 
+import authStore from "@/store/authStore";
+
 const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-export const axiosInstance = axios.create({
+const axiosInstance = axios.create({
   baseURL: baseURL ?? "",
+   headers: {
+    'Content-Type': 'application/json',
+  },
 });
+
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const authState = authStore.getState();
+    const token = authState ? authState.accessToken : null;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export {
+  axiosInstance
+}
